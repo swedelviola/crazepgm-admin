e1327b1910a058756e677539193c9592f0566daa
swedelviola <59990454+swedelviola@users.noreply.github.com>
2020-03-23 12:49:09 +0530

    Update addcomp8.php

--- addcomp8.php
+++ addcomp8.php
@@ -50,7 +50,7 @@
   <a href="changewei.php">Change weightage</a><br>
   <a href="checkwei.php">Current weightage</a><br>
   <a href="feedback.php">Feedback</a><br>
-  <a href="">Edit</a><br>
+  <a href="edit.php">Edit</a><br>
 </div>
 <br>
 
