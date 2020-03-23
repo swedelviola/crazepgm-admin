e2d59cdbab3a919d59bb99ab52c487fd4c04f958
swedelviola <59990454+swedelviola@users.noreply.github.com>
2020-03-23 12:47:58 +0530

    Update addcomp5.php

--- addcomp5.php
+++ addcomp5.php
@@ -46,7 +46,7 @@ session_start();
   <a href="changewei.php">Change weightage</a><br>
   <a href="checkwei.php">Current weightage</a><br>
   <a href="feedback.php">Feedback</a><br>
-  <a href="">Edit</a><br>
+  <a href="edit.php">Edit</a><br>
 </div>
 <br>
 <?php
