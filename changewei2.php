11810235cc6e37476efe7129a6a92cea0e4f8353
swedelviola <59990454+swedelviola@users.noreply.github.com>
2020-03-23 12:50:29 +0530

    Update changewei2.php

--- changewei2.php
+++ changewei2.php
@@ -48,7 +48,7 @@ $_SESSION['ename']= $_POST['events'];
   <a href="changewei.php" id="hell" active>Change weightage</a><br>
   <a href="checkwei.php">Current weightage</a><br>
   <a href="feedback.php">Feedback</a><br>
-  <a href="">Edit</a><br>
+  <a href="edit.php">Edit</a><br>
 </div>
 <br>
 
