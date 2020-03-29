cfe26c13df8c7ebba602f276983a59c659e0d065
swedelviola <59990454+swedelviola@users.noreply.github.com>
2020-03-29 23:06:21 +0530

    Delete editp1.php

--- editp1.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require 'connection.php';
-	$e_id=$_POST['radio'];
-	$sql="UPDATE event set defaults=3 where e_id='$e_id'";
-	$result=mysqli_query($conn,$sql) or die(mysqli_error($conn));
-	header("Location: edit.php");
-
-?>
